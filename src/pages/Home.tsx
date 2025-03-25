2adc706066c54c54bf0af52c7f9a5c542402c29f
yerr04 <michaelkmwaura@gmail.com>
2025-03-25 19:31:09 -0400

    Added tailwindcss to config Added searchbar to landing page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,12 @@
+import React from 'react'
+import { FaSearch } from 'react-icons/fa'
+
 export function Home() {
-    return <h1>Home</h1>
+    return (
+        <form className='bg-[#29334D] p-2 rounded-full flex items-center'>
+            <input type='text' placeholder='Search'
+            className='bg-transparent focus:outline-none w-24 sm:w-64 ml-3 mr-auto placeholder-gray-600'/>
+            <FaSearch className='text-gray-500 mr-3' />
+        </form>
+    )
 }
